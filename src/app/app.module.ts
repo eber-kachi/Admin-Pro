09f65871883ee412fce5b535d280d14bae33a20a
Eber Kachi <eber.nina96@gmail.com>
2019-10-27 17:33:42 -0400

    quitanto erroes de tslin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,16 @@
-import {BrowserModule} from "@angular/platform-browser";
-import {NgModule} from "@angular/core";
-//ROUTES
-import {APP_ROUTES} from "./app.routes";
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+// ROUTES
+import {APP_ROUTES} from './app.routes';
 // Modulos
 import {PagesModule} from './pages/pages.module';
 
 
-import {AppComponent} from "./app.component";
-import {LoginComponent} from "./login/login.component";
-//componentes 
+import {AppComponent} from './app.component';
+import {LoginComponent} from './login/login.component';
+// componentes
 import {RegisterComponent} from './login/register.component';
-import {IncrementadorComponent} from "./components/incrementador/incrementador.component";
-import {FormsModule} from "@angular/forms";
+import {FormsModule} from '@angular/forms';
 
 
 @NgModule({
--- src/app/pages/pages.routes.ts
+++ src/app/pages/pages.routes.ts
@@ -1,4 +1,4 @@
-import { Routes, RouterModule } from "@angular/router";
+import { Routes, RouterModule } from '@angular/router';
 
 import { PagesComponent } from './pages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -9,12 +9,12 @@ import { Graficas1Component } from './graficas1/graficas1.component';
 
 const pagesRoutes: Routes = [
     {
-        path: "",
+        path: '',
         component: PagesComponent,
         children: [
             { path: "dashboard", component: DashboardComponent },
             { path: "progress", component: ProgressComponent },
-            { path: "graficas1", component: Graficas1Component },
+            { path: 'graficas1', component: Graficas1Component },
             { path: "", redirectTo: "/dashboard", pathMatch: "full" }
         ]
     },
