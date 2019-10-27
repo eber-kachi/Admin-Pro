5a38b7332f6de142055e6c92ee65527af1a48eea
Eber Kachi <eber.nina96@gmail.com>
2019-10-27 17:56:14 -0400

    @input de un componente a otro

--- src/app/components/incrementador/incrementador.component.ts
+++ src/app/components/incrementador/incrementador.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,Input } from '@angular/core';
 
 @Component({
   selector: 'app-incrementador',
@@ -6,8 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class IncrementadorComponent implements OnInit {
-  leyenda: string;
-  progreso: number;
+  @Input('nombre') leyenda: string;
+  @Input() progreso: number;
   constructor() {
     this.leyenda = 'Leyenda';
     this.progreso = 20;
