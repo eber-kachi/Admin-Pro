7b885376c3d51cb521a5fd2515280bcec3c68784
Eber Kachi <eber.nina96@gmail.com>
2019-10-30 18:12:24 -0400

    graficos ng2-chart importando modulo

--- src/app/pages/graficas1/graficas1.component.ts
+++ src/app/pages/graficas1/graficas1.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-graficas1',
   templateUrl: './graficas1.component.html',
   styles: []
 })
 export class Graficas1Component implements OnInit {
+  public doughnutcharType: string='doughnut';
+  public doughnutchartLabels: string[]=['Hombres','Mujeres','medios'];
+  public doughnutchartData:number[]=[60,10,78];
+
+graficos:any={
+  'grafico1':{
+    'labels':['con frijoles','con natilla','con tocino'],
+    'data':[24,30,46],
+    'type':'doughnut',
+    'leyenda':'el pan se come con '
+  },
+  'grafico2':{
+    'labels':['Hombres','Mujeres'],
+    'data':[4500,6000],
+    'type':'doughnut',
+    'leyenda':'Entrevistados'
+  },
+  'grafico3':{
+    'labels':['Si','No'],
+    'data':[95,5],
+    'type':'doughnut',
+    'leyenda':'Le dan gases los frioles'
+  },
+  'grafico4':{
+    'labels':['Si','No'],
+    'data':[85,15],
+    'type':'doughnut',
+    'leyenda':'Tiene sexo 1 diario'
+  },
+};
 
   constructor() { }
 
   ngOnInit() {
   }
+
 
 }
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -4,12 +4,15 @@ import {SharedModule} from '../shared/shared.module';
 import {PagesComponent} from './pages.component';
 // RUTAS HIJAS
 import {PAGES_ROUTES} from './pages.routes';
+ // ng2-chats
+import { ChartsModule } from 'ng2-charts';
 
 import {DashboardComponent} from "./dashboard/dashboard.component";
 import {ProgressComponent} from "./progress/progress.component";
 import {Graficas1Component} from "./graficas1/graficas1.component";
 import {FormsModule} from "@angular/forms";
 import {IncrementadorComponent} from "../components/incrementador/incrementador.component";
+import {GraficodonaComponent} from "../components/graficodona/graficodona.component";
 
 @NgModule({
   declarations: [
@@ -17,7 +20,8 @@ import {IncrementadorComponent} from "../components/incrementador/incrementador.
     DashboardComponent,
     ProgressComponent,
     Graficas1Component,
-    IncrementadorComponent
+    IncrementadorComponent,
+    GraficodonaComponent
   ],
   exports: [// para que de puedan acceder desde cualquier logar
     PagesComponent,
@@ -28,6 +32,7 @@ import {IncrementadorComponent} from "../components/incrementador/incrementador.
     SharedModule,
     PAGES_ROUTES, // sus propias rutas
     FormsModule,
+    ChartsModule
     // para que funcione los formularos o el NGmodul
   ]
 })
